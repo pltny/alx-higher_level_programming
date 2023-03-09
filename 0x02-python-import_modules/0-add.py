c3da59a70d53293a765ec7b113f7b842e483ad0f
Derick Gitonga <gitongaderick17@gmail.com>
2023-03-09 11:43:51 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if_name_=="_main_":
-from add_0 import add
-a = 1
-b = 2
-result = add(a,b)
-print("{} + {} = {}",.format(a , b , result)
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a,b)
+    print("{} + {} = {}",.format(a , b , result)
