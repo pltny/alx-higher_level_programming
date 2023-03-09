4762c6f015a580cea600f6d2a66f2bced6fe0b3b
Derick Gitonga <gitongaderick17@gmail.com>
2023-03-09 12:10:15 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
 def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}",.format(a ,b ,result)
