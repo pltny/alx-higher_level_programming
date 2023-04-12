1ac12acf0e0d4bd43827a58a475eab526054e0e5
Derick Gitonga <gitongaderick17@gmail.com>
2023-04-12 14:22:22 -0700

    returns json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A function that returns  the JSON representation of string"""
+import json
+
+
+def to_json_string(my_obj):
+    """ function that returns  the JSON representation of string"""
+    return json.dumps(my_obj)
