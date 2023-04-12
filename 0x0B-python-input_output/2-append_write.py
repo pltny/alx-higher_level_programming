176c810e0e1eb4bbd0467da1be49e1f169d1f9e0
Derick Gitonga <gitongaderick17@gmail.com>
2023-04-12 14:01:55 -0700

    python input

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""Defines a file-appending function."""
 
 
 def append_write(filename="", text=""):
+    """Appends a string.
+    Args:
+        filename (str): name of the file 
+        text (str):string
+    Returns:
+        The 
+    """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
