f0763fa9b5d68bbb9678ba0bce57aa0ae1f834c5
Derick Gitonga <gitongaderick17@gmail.com>
2023-05-17 11:29:20 -0700

    updated

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,7 @@
--- lists all shows that
--- lists  row
-SELECT tv_shows.title, tv_show_genres.genre_iid FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- creates the database 
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants privileges t
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,3 @@
+-- creates table
+-- creates database
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);i
