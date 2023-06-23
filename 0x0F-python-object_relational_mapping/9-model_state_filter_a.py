9d650e1bf353cbaadda6c33ceb7ffa706cca9137
Derick Gitonga <gitongaderick17@gmail.com>
2023-06-23 16:22:22 -0700

    update9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State
-from the database
-Usage: ./9-model_state_filter_a.py <mysql username> /
+<mysql username> /
                                    <mysql password> /
                                    <database name>
 """
