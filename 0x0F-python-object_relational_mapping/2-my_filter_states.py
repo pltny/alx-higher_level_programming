e447ea817216617c62fca7e2c99e17fdcc2701c2
Derick Gitonga <gitongaderick17@gmail.com>
2023-06-23 15:56:36 -0700

    update

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" <mysql username>
-     <mysql password>
-    <database name>
-     <name searched>
+""" takes
+    hbtnre name matches the argument
+     Usage: ./2-my_filter_states.py <mysql username>
+                                    <mysql password>
+                                    <database name>
+                                    <state name searched>
 """
 import sys
 import MySQLdb
@@ -11,6 +13,6 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("""SELECT * FROM states
-                WHERE name LIKE 
-                ORDER BY """.format(sys.argv[4]).strip("'"))
+                WHERE name LIKE BINARY '{}'
+                ORDER BY states.id ASC""".format(sys.argv[4]).strip("'"))
     [print(state) for state in c.fetchall()]i
